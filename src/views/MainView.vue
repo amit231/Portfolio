e65c7db93fff1e2cb49bc2a25cb0903e18a4a024
amit sharma <45491011+amit231@users.noreply.github.com>
2024-08-04 19:29:29 +0530

    Update MainView.vue

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,42 +1,42 @@
 <template>
 	<SplashView v-if="showingSplash"/>
 
-  	<div v-show="!showingSplash" class="min-h-screen overscroll-contain bg-white dark:bg-slate-900 dark:text-slate-300 text-base md:text-xl">
+  	<div v-show="!showingSplash" class="min-h-screen text-base bg-white overscroll-contain dark:bg-slate-900 dark:text-slate-300 md:text-xl">
 		<Navbar :show-transition="showLanding" :dark-mode-active="darkModeActive" @toggle-dark="toggleDark"/>
 
 		<div class="min-h-full mx-10">
-			<div class="flex flex-col md:grid md:grid-cols-6 min-h-full">
-				<div class="col-span-1 flex-initial relative mx-auto md:mx-0 order-2 md:order-none">
-					<ul class="md:fixed md:bottom-0 mb-7 flex items-center space-x-8 md:flex-col md:space-y-4 md:space-x-0">
+			<div class="flex flex-col min-h-full md:grid md:grid-cols-6">
+				<div class="relative flex-initial order-2 col-span-1 mx-auto md:mx-0 md:order-none">
+					<ul class="flex items-center space-x-8 md:fixed md:bottom-0 mb-7 md:flex-col md:space-y-4 md:space-x-0">
 						<li v-show="githubLink" :class="['transition-all motion-reduce:transition-none duration-500', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
 							<a :href="githubLink" target="_blank">
-								<font-awesome-icon icon="fa-brands fa-github" class="h-8 w-8 md:h-12 md:w-12 text-black transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300 dark:text-slate-300"></font-awesome-icon>
+								<font-awesome-icon icon="fa-brands fa-github" class="w-8 h-8 text-black transition duration-300 ease-out md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300"></font-awesome-icon>
 							</a>
 						</li>
 						<li v-show="linkedinLink" :class="['transition-all motion-reduce:transition-none duration-500 delay-[100ms]', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
 							<a :href="linkedinLink" target="_blank">
-								<font-awesome-icon icon="fa-brands fa-linkedin" class="h-8 w-8 md:h-12 md:w-12 text-black transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300 dark:text-slate-300"></font-awesome-icon>
+								<font-awesome-icon icon="fa-brands fa-linkedin" class="w-8 h-8 text-black transition duration-300 ease-out md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300"></font-awesome-icon>
 							</a>
 						</li>
 						<li v-show="mediumLink" :class="['transition-all motion-reduce:transition-none duration-500 delay-[150ms]', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
 							<a :href="mediumLink" target="_blank">
-								<font-awesome-icon icon="fa-brands fa-medium" class="h-8 w-8 md:h-12 md:w-12 text-black transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300 dark:text-slate-300"></font-awesome-icon>
+								<font-awesome-icon icon="fa-brands fa-medium" class="w-8 h-8 text-black transition duration-300 ease-out md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300"></font-awesome-icon>
 							</a>
 						</li>
 						<li v-show="stackoverflowLink" :class="['transition-all motion-reduce:transition-none duration-500 delay-[200ms]', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
 							<a :href="stackoverflowLink" target="_blank">
-								<font-awesome-icon icon="fa-brands fa-stack-overflow" class="h-8 w-8 md:h-12 md:w-12 text-black transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300 dark:text-slate-300"></font-awesome-icon>
+								<font-awesome-icon icon="fa-brands fa-stack-overflow" class="w-8 h-8 text-black transition duration-300 ease-out md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300"></font-awesome-icon>
 							</a>
 						</li>
 						<li v-show="xTwitterLink" :class="['transition-all motion-reduce:transition-none duration-500 delay-[200ms]', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
 							<a :href="xTwitterLink" target="_blank">
-								<font-awesome-icon icon="fa-brands fa-x-twitter" class="h-8 w-8 md:h-12 md:w-12 text-black transition ease-out hover:-translate-y-1 motion-reduce:hover:translate-y-0 duration-300 dark:text-slate-300"></font-awesome-icon>
+								<font-awesome-icon icon="fa-brands fa-x-twitter" class="w-8 h-8 text-black transition duration-300 ease-out md:h-12 md:w-12 hover:-translate-y-1 motion-reduce:hover:translate-y-0 dark:text-slate-300"></font-awesome-icon>
 							</a>
 						</li>
 					</ul>
 				</div>
-				<div class="col-span-4 flex-1 flex-col order-1 md:order-none">
-					<div class="container mx-auto max-w-full 2xl:max-w-6xl">
+				<div class="flex-col flex-1 order-1 col-span-4 md:order-none">
+					<div class="container max-w-full mx-auto 2xl:max-w-6xl">
 						<LandingView :content="portfolio.greeting" :show-transition="showLanding"/>
 
 						<AboutView :content="portfolio.about" :transitions="portfolio.transitions" />
@@ -47,17 +47,17 @@
 
 						<ContactView :content="portfolio.contact" :transitions="portfolio.transitions" />
 
-						<footer class="hidden text-center md:block mb-5">
-							<p>🚀 Designed & Created by <a href="https://maximshelepov.com" class="hover:text-link-color" target="_blank">Maxim Shelepov</a></p>
+						<footer class="hidden mb-5 text-center md:block">
+							<p>🚀 Designed & Created by <a href="https://www.linkedin.com/in/engineer-amit/" class="hover:text-link-color" target="_blank">Amit Sharma</a></p>
 							<a href="https://github.com/feifyKike/webdev_portfolio" class="hover:text-link-color" target="_blank">Code available on Github 👾</a>
 						</footer>
 					</div>
 				</div>
-				<div class="hidden col-span-1 md:flex flex-initial relative order-2 text-center md:order-none">
+				<div class="relative flex-initial order-2 hidden col-span-1 text-center md:flex md:order-none">
 					<p :class="['fixed bottom-2 right-10 origin-top-right rotate-90 transition-all motion-reduce:transition-none duration-500', showLanding ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">📍 Based in {{ portfolio.greeting.basedLocation }}</p>
 				</div>
-				<footer class="block text-center text-sm md:hidden order-last mb-5">
-					<p>🚀 Designed & Created by <a href="https://maximshelepov.com" class="hover:text-link-color" target="_blank">Maxim Shelepov</a></p>
+				<footer class="order-last block mb-5 text-sm text-center md:hidden">
+					<p>🚀 Designed & Created by <a href="https://www.linkedin.com/in/engineer-amit/" class="hover:text-link-color" target="_blank">Amit Sharma</a></p>
 					<a href="https://github.com/feifyKike/webdev_portfolio" class="hover:text-link-color" target="_blank">Code available on Github 👾</a>
 				</footer>
 			</div>
